3cb0c491deb2b3a07988f3eca966b3823612543e
João Vitor <joaof-neiva@hotmail.com>
2018-10-07 13:02:01 -0300

    <<Tela de cadastrar Login>>

---
+++ Telas/Entregavel I/Login/frmCadastrarLogin.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Catiotro_s.Telas.Entregavel_I.Login
+{
+    partial class frmCadastrarLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ckbConsultar = new System.Windows.Forms.CheckBox();
+            this.ckbCadastar = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ckbAdm = new System.Windows.Forms.CheckBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtConfirm = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(339, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 21);
+            this.label6.TabIndex = 59;
+            this.label6.Text = "X";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(125, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 39);
+            this.button1.TabIndex = 58;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ckbConsultar
+            // 
+            this.ckbConsultar.AutoSize = true;
+            this.ckbConsultar.Location = new System.Drawing.Point(239, 189);
+            this.ckbConsultar.Name = "ckbConsultar";
+            this.ckbConsultar.Size = new System.Drawing.Size(70, 17);
+            this.ckbConsultar.TabIndex = 57;
+            this.ckbConsultar.Text = "Consultar";
+            this.ckbConsultar.UseVisualStyleBackColor = true;
+            // 
+            // ckbCadastar
+            // 
+            this.ckbCadastar.AutoSize = true;
+            this.ckbCadastar.Location = new System.Drawing.Point(167, 189);
+            this.ckbCadastar.Name = "ckbCadastar";
+            this.ckbCadastar.Size = new System.Drawing.Size(71, 17);
+            this.ckbCadastar.TabIndex = 56;
+            this.ckbCadastar.Text = "Cadastrar";
+            this.ckbCadastar.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(8, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 21);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "Permissões";
+            // 
+            // ckbAdm
+            // 
+            this.ckbAdm.AutoSize = true;
+            this.ckbAdm.Location = new System.Drawing.Point(111, 189);
+            this.ckbAdm.Name = "ckbAdm";
+            this.ckbAdm.Size = new System.Drawing.Size(50, 17);
+            this.ckbAdm.TabIndex = 54;
+            this.ckbAdm.Text = "ADM";
+            this.ckbAdm.UseVisualStyleBackColor = true;
+            this.ckbAdm.CheckedChanged += new System.EventHandler(this.ckbAdm_CheckedChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(146, 151);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(186, 20);
+            this.txtEmail.TabIndex = 53;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(146, 115);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(186, 20);
+            this.txtNome.TabIndex = 52;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(146, 52);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(186, 20);
+            this.txtSenha.TabIndex = 51;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(79, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 21);
+            this.label4.TabIndex = 50;
+            this.label4.Text = "E-mail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 21);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Nome Completo";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(146, 20);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(186, 20);
+            this.txtUsuario.TabIndex = 48;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 21);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Senha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 21);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Usuário";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 21);
+            this.label7.TabIndex = 60;
+            this.label7.Text = "Confirmar Senha";
+            // 
+            // txtConfirm
+            // 
+            this.txtConfirm.Location = new System.Drawing.Point(146, 84);
+            this.txtConfirm.Name = "txtConfirm";
+            this.txtConfirm.PasswordChar = '*';
+            this.txtConfirm.Size = new System.Drawing.Size(186, 20);
+            this.txtConfirm.TabIndex = 61;
+            // 
+            // frmCadastrarLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 268);
+            this.Controls.Add(this.txtConfirm);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ckbConsultar);
+            this.Controls.Add(this.ckbCadastar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ckbAdm);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCadastrarLogin";
+            this.Text = "frmCadastrarLogin";
+            this.Load += new System.EventHandler(this.frmCadastrarLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox ckbConsultar;
+        private System.Windows.Forms.CheckBox ckbCadastar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox ckbAdm;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtConfirm;
+    }
+}
---
+++ Telas/Entregavel I/Login/frmCadastrarLogin.cs
@@ -0,0 +1,70 @@
+﻿using Catiotro_s.classes.Classes.Login;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Catiotro_s.Telas.Entregavel_I.Login
+{
+    public partial class frmCadastrarLogin : Form
+    {
+        public frmCadastrarLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (txtSenha.Text == txtConfirm.Text)
+            {
+                LoginDTO dto = new LoginDTO();
+                dto.Nome = txtUsuario.Text;
+                dto.Senha = txtSenha.Text;
+                dto.NmUsuario = txtNome.Text;
+                dto.Email = txtEmail.Text;
+                dto.PermicaoADM = ckbAdm.Checked;
+                dto.PermicaoCadastro = ckbCadastar.Checked;
+                dto.PermicaoConsulta = ckbConsultar.Checked;
+
+                LoginBusiness buss = new LoginBusiness();
+                buss.Salvar(dto);
+
+                MessageBox.Show("Novo usuário criado com sucesso!", "Catioro's", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("Senha incorreta", "Catioro's", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }         
+        }
+
+        private void frmCadastrarLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ckbAdm_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckbAdm.Checked == true)
+            {
+                ckbCadastar.Checked = true;
+                ckbConsultar.Checked = true;
+            }
+            else if (ckbConsultar.Checked == false)
+            {
+                ckbCadastar.Checked = false;
+                ckbConsultar.Checked = false;
+            }
+        }
+    }
+}
