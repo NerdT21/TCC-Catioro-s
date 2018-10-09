0d0b88ae23b99115f8e0770a70fcc71af685e41b
INSTITUTO <INSTITUTO@INFO21>
2018-10-09 16:24:35 -0300

    Fornecedor

--- classes/Classes/Fornecedores/FonecedoresDTO.cs
+++ classes/Classes/Fornecedores/FonecedoresDTO.cs
@@ -10,16 +10,16 @@ namespace Catiotro_s.classes.Classes.Cliente
     {
         public int Id { get; set; }
         public int IdEstado { get; set; }
-        public string Empresa { get; set; }
-        public string NmFantasia { get; set; }
-        public int Cnpj { get; set; }
-        public string Endereco { get; set; }
-        public int Numero { get; set; }
+        public string Nome { get; set; }
+        
+        public string Cnpj { get; set; }
+       
+        
         public string Bairro { get; set; }
         public string Cidade { get; set; }
-        public int Cpf { get; set; }
-        public int Telefone { get; set; }
+       
+        public string Telefone { get; set; }
         public string Email { get; set; }
-        public string Local { get; set; }
+       
     }
 }
