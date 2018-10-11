58ae47402e75b0df550d1e6c9e2f1527211036e2
infod32 <infod32@INFO19>
2018-10-11 17:49:52 -0300

    {{Dto Cliente}}

--- classes/Classes/AddConsultar/Cliente/ClienteDTO.cs
+++ classes/Classes/AddConsultar/Cliente/ClienteDTO.cs
@@ -8,17 +8,36 @@ namespace Catiotro_s.classes.Classes.Cliente
 {
      public class ClienteDTO
     {
-        public int Pessoa { get; set; }
+        public int id { get; set; }
+
         public int IdEstado { get; set; }
+
         public string Nome { get; set; }
-        public int CPF { get; set; }
-        public string Endereco { get; set; }
-        public string Local { get; set; }
-        public string Cidade { get; set; }
-        public int CEP { get; set; }
+
         public string Email { get; set; }
+
+        public int Cpf { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Local { get; set; }
+
+        public string Cidade { get; set; }
+
+        public int Cep { get; set; }
+
         public int Telefone { get; set; }
-        public string DataNasc { get; set; }
+
+        public string DataNascimento { get; set; }
+
         public string DataCadastro { get; set; }
+
+        public string Rg { get; set; }
+
+        public string Bairro { get; set; }
+
+        public int NumeroCasa { get; set; }
+
+
     }
 }
