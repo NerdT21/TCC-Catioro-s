d4c97b3c218f5844f0e9b4421fe9ec78c576232d
Raian Chaveiro <chaveiro.raian@gmail.com>
2018-10-12 02:12:18 -0300

    -Design Menu

--- Telas/Menu.Designer.cs
+++ Telas/Menu.Designer.cs
@@ -144,7 +144,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(63, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(62, 4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(440, 394);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
