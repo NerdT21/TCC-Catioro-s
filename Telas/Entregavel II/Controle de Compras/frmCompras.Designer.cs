6e6ccbdf7bf4165a75114567497c9b9f74c325b9
Raian Chaveiro <chaveiro.raian@gmail.com>
2018-10-13 16:04:19 -0300

    -Compras Design

--- Telas/Entregavel II/Controle de Compras/frmCompras.Designer.cs
+++ Telas/Entregavel II/Controle de Compras/frmCompras.Designer.cs
@@ -28,18 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cboProduto = new System.Windows.Forms.ComboBox();
+            this.cboFornecedor = new System.Windows.Forms.ComboBox();
+            this.nudQuantidade = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProduto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboTipoPag = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nudPreco = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvCompras = new System.Windows.Forms.DataGridView();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.mkbDataCompra = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantidade)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPreco)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompras)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cboProduto
+            // 
+            this.cboProduto.FormattingEnabled = true;
+            this.cboProduto.Location = new System.Drawing.Point(180, 52);
+            this.cboProduto.Name = "cboProduto";
+            this.cboProduto.Size = new System.Drawing.Size(140, 23);
+            this.cboProduto.TabIndex = 0;
+            // 
+            // cboFornecedor
+            // 
+            this.cboFornecedor.FormattingEnabled = true;
+            this.cboFornecedor.Location = new System.Drawing.Point(422, 55);
+            this.cboFornecedor.Name = "cboFornecedor";
+            this.cboFornecedor.Size = new System.Drawing.Size(140, 23);
+            this.cboFornecedor.TabIndex = 1;
+            this.cboFornecedor.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // nudQuantidade
+            // 
+            this.nudQuantidade.Location = new System.Drawing.Point(180, 166);
+            this.nudQuantidade.Name = "nudQuantidade";
+            this.nudQuantidade.Size = new System.Drawing.Size(118, 23);
+            this.nudQuantidade.TabIndex = 2;
+            this.nudQuantidade.ValueChanged += new System.EventHandler(this.nudQuantidade_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(341, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fornecedor";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(69, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Data de Compra";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(35, 246);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(139, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Forma De Pagamento ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtProduto
+            // 
+            this.txtProduto.Location = new System.Drawing.Point(180, 129);
+            this.txtProduto.Name = "txtProduto";
+            this.txtProduto.Size = new System.Drawing.Size(116, 23);
+            this.txtProduto.TabIndex = 11;
+            this.txtProduto.TextChanged += new System.EventHandler(this.txtProduto_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(119, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Produto";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // cboTipoPag
+            // 
+            this.cboTipoPag.FormattingEnabled = true;
+            this.cboTipoPag.Location = new System.Drawing.Point(180, 245);
+            this.cboTipoPag.Name = "cboTipoPag";
+            this.cboTipoPag.Size = new System.Drawing.Size(140, 23);
+            this.cboTipoPag.TabIndex = 13;
+            this.cboTipoPag.SelectedIndexChanged += new System.EventHandler(this.cboTipoPag_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(397, 321);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Preço";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // nudPreco
+            // 
+            this.nudPreco.Location = new System.Drawing.Point(444, 319);
+            this.nudPreco.Name = "nudPreco";
+            this.nudPreco.Size = new System.Drawing.Size(118, 23);
+            this.nudPreco.TabIndex = 14;
+            this.nudPreco.ValueChanged += new System.EventHandler(this.nudPreco_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(98, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Quantidade";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(22, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(648, 5);
+            this.panel1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Produto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dgvCompras
+            // 
+            this.dgvCompras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCompras.Location = new System.Drawing.Point(396, 129);
+            this.dgvCompras.Name = "dgvCompras";
+            this.dgvCompras.Size = new System.Drawing.Size(247, 162);
+            this.dgvCompras.TabIndex = 17;
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComprar.Location = new System.Drawing.Point(180, 306);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(97, 32);
+            this.btnComprar.TabIndex = 18;
+            this.btnComprar.Text = "button1";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // mkbDataCompra
+            // 
+            this.mkbDataCompra.Location = new System.Drawing.Point(182, 206);
+            this.mkbDataCompra.Mask = "00/00/0000";
+            this.mkbDataCompra.Name = "mkbDataCompra";
+            this.mkbDataCompra.Size = new System.Drawing.Size(65, 23);
+            this.mkbDataCompra.TabIndex = 19;
+            this.mkbDataCompra.ValidatingType = typeof(System.DateTime);
             // 
             // frmCompras
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.mkbDataCompra);
+            this.Controls.Add(this.btnComprar);
+            this.Controls.Add(this.dgvCompras);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.nudPreco);
+            this.Controls.Add(this.cboTipoPag);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtProduto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudQuantidade);
+            this.Controls.Add(this.cboFornecedor);
+            this.Controls.Add(this.cboProduto);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmCompras";
-            this.Size = new System.Drawing.Size(583, 405);
+            this.Size = new System.Drawing.Size(680, 467);
+            this.Load += new System.EventHandler(this.frmCompras_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantidade)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPreco)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompras)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboProduto;
+        private System.Windows.Forms.ComboBox cboFornecedor;
+        private System.Windows.Forms.NumericUpDown nudQuantidade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProduto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboTipoPag;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nudPreco;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvCompras;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.MaskedTextBox mkbDataCompra;
     }
 }
--- Telas/Entregavel II/Controle de Compras/frmCompras.cs
+++ Telas/Entregavel II/Controle de Compras/frmCompras.cs
@@ -16,5 +16,80 @@ namespace Catiotro_s.Telas.Entregavel_II.Controle_de_Compras
         {
             InitializeComponent();
         }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCompras_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudQuantidade_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudPreco_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboTipoPag_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtProduto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnComprar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
